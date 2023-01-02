7bf23820aca1f006b763f7fa8e56d423ab87d3c7
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2023-01-02 18:30:45 -0300

    feat: inicializa o projeto

---
+++ src/test/java/br/com/project/PharmacyManagement/PharmacyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.PharmacyManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
