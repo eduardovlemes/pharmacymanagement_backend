1b0289fce5fff3f2e18909bd0460ce47d3c5c778
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2023-01-04 23:07:14 -0300

    PM-1A feat: cria GET usuario login

--- src/main/java/br/com/project/PharmacyManagement/repository/UserEntityRepository.java
+++ src/main/java/br/com/project/PharmacyManagement/repository/UserEntityRepository.java
@@ -4,6 +4,10 @@ import br.com.project.PharmacyManagement.model.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserEntityRepository extends CrudRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findUserByEmail(String email);
 }
