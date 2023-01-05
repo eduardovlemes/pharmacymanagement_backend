eea202dd3afd16cbf24047346f6c54d63f702f97
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2023-01-05 11:13:39 -0300

    PM-3A feat: cria GET todos medicamentos

--- src/main/java/br/com/project/PharmacyManagement/controller/DrugEntityController.java
+++ src/main/java/br/com/project/PharmacyManagement/controller/DrugEntityController.java
@@ -4,10 +4,9 @@ package br.com.project.PharmacyManagement.controller;
 import br.com.project.PharmacyManagement.model.DrugEntity;
 import br.com.project.PharmacyManagement.service.DrugEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicamentos")
@@ -18,4 +17,7 @@ public class DrugEntityController {
 
     @PostMapping
     public void register(@RequestBody DrugEntity drugEntity){drugEntityService.save(drugEntity);}
+
+    @GetMapping
+    public List<DrugEntity> listALl() {return drugEntityService.findAll();}
 }
