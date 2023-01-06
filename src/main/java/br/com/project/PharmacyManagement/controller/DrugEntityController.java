f3136493c3480711469cc49c271dc0b6fb7f426b
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2023-01-05 22:30:24 -0300

    PM-3E feat: cria o DELETE de medicamento e corrige o PUT

--- src/main/java/br/com/project/PharmacyManagement/controller/DrugEntityController.java
+++ src/main/java/br/com/project/PharmacyManagement/controller/DrugEntityController.java
@@ -1,9 +1,12 @@
 package br.com.project.PharmacyManagement.controller;
 
 
+import br.com.project.PharmacyManagement.DTO.DrugDTO;
 import br.com.project.PharmacyManagement.model.DrugEntity;
 import br.com.project.PharmacyManagement.service.DrugEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,14 +19,20 @@ public class DrugEntityController {
     DrugEntityService drugEntityService;
 
     @PostMapping
-    public void register(@RequestBody DrugEntity drugEntity){drugEntityService.save(drugEntity);}
+    public void register(@RequestBody DrugEntity drugEntity){drugEntityService.saveDrug(drugEntity);}
 
     @GetMapping
-    public List<DrugEntity> listALl() {return drugEntityService.findAll();}
+    public List<DrugEntity> listALl() {return drugEntityService.getAll();}
 
     @GetMapping("/{id}")
-    public DrugEntity getById (@PathVariable Long id){return drugEntityService.findById(id);}
+    public DrugEntity getById (@PathVariable Long id){return drugEntityService.getById(id);}
 
-    @PutMapping
-    public void update(@RequestBody DrugEntity drugEntity){drugEntityService.save(drugEntity);}
+    @PutMapping("/{id}")
+    public ResponseEntity<Long> update(@PathVariable Long id, @RequestBody DrugDTO drugDTO){
+        drugEntityService.update(id, drugDTO);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){drugEntityService.delete(id);}
 }
