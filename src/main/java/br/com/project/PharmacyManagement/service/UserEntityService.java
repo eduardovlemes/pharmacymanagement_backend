69c460d1707027b7015a6dc53b0e2369b2fed427
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2023-01-09 23:39:27 -0300

    PM-5B feat: trata erros do endpoint /usuario

--- src/main/java/br/com/project/PharmacyManagement/service/UserEntityService.java
+++ src/main/java/br/com/project/PharmacyManagement/service/UserEntityService.java
@@ -1,7 +1,10 @@
 package br.com.project.PharmacyManagement.service;
 
+import br.com.project.PharmacyManagement.DTO.UserDTO;
 import br.com.project.PharmacyManagement.model.UserEntity;
 import br.com.project.PharmacyManagement.repository.UserEntityRepository;
+import br.com.project.PharmacyManagement.service.exception.ServerSideException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +14,34 @@ public class UserEntityService {
     @Autowired
     private UserEntityRepository userEntityRepository;
 
-    public void save(UserEntity userEntity){
-        userEntityRepository.save(userEntity);
+//    public void save(UserEntity userEntity){
+//        userEntityRepository.save(userEntity);
+//    }
+
+    public void saveUser (UserDTO userDTO){
+
+        try {
+            UserEntity user = new UserEntity();
+            user.setEmail(userDTO.getEmail());
+            user.setPassword(userDTO.getPassword());
+
+            userEntityRepository.save(user);
+        } catch (Exception e){
+            throw new ServerSideException("Erro ao salvar o usuário: " + e.getLocalizedMessage());
+        }
+
     }
 
-    public UserEntity findByEmail(String email) {
-        return userEntityRepository.findUserByEmail(email).get();
+//    public UserEntity findByEmail(String email) {
+//        return userEntityRepository.findUserByEmail(email).get();
+//    }
+    public Long findByEmailAndPassword(UserDTO userDTO) {
+
+        UserEntity user = userEntityRepository.findByEmailAndPassword(userDTO.getEmail(), userDTO.getPassword());
+
+        if (user == null) {
+            throw new EntityNotFoundException("Usuário ou senha não encontrado.");
+        }
+        return user.getId();
     }
 }
